bb1965bafde75bd0555764827df83ad312f9c7e7
nolisio <57495289+nolisio@users.noreply.github.com>
2019-11-08 13:26:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,7 @@ async def on_command_error(ctx, error):
     await ctx.send(str(error))  
     
 @bot.command()
-async def how are you?(ctx):
+async def how are you(ctx):
     await ctx.send('可もなく不可もなくといった平常運転です')
     
 bot.run(token)
