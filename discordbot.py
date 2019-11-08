de69bdc61d1c03b1542fac79371d053a374cc68b
nolisio <57495289+nolisio@users.noreply.github.com>
2019-11-08 23:18:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='めいどちゃん、')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -15,6 +15,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def おはよう(ctx):
     await ctx.send('おはようございます')
+    
+@bot.command()
+async def 調子はどう？(ctx):
+    await ctx.send('可もなく不可もなくといった感じです')
 
 
 bot.run(token)
